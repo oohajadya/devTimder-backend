aa534814045643aa7d96f7ee9831da67b1ab39d7
oohajadya <72297271+oohajadya@users.noreply.github.com>
2025-07-23 21:34:11 +0530

    exploring REST apis

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+//exploring the REST APIS
+app.get('/user',(req,res) => {
+   res.send('get request')
+})
+
+app.post('/user',(req,res) => {
+   res.send('saved request')
+})
+
+app.put('/user',(req,res) => {
+   res.send('updated request')
+})
+
+app.delete('/user',(req,res) => {
+   res.send('delte request')
+})
+
+
+
 app.listen(3000,() => {
     console.log('app is running in the port 3000')
 })
